af350cee76c0887ff19f11cc68f9edc7b2e00245
Johankivilompolo <Johankivi@hotmail.com>
2022-05-30 03:07:22 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'inc/functions.php';
+require_once 'inc/headers.php';
+
+try {
+$db = new PDO('mysql:host=localhost;dbname=shoppinglist;charset=utf8','root');
+$sql = "select * from item";
+$query = $db->query($sql);
+$results = $query->fetchAll(PDO::FETCH_ASSOC); 
+header('HTTP/1.1 200 OK');
+print json_encode($results);
+} catch (PDOExpeption $pdoex) {
+    returnError($pdoex);
+}
